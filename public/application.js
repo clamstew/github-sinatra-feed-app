1ac9cc06060f37d30c88c1d3d78214932f1999e5
Clay Stewart <claytonmstewart@gmail.com>
2014-02-19 23:37:55 -0600

    Add closer js button the the ads on the right side

--- public/application.js
+++ public/application.js
@@ -1,1 +1,6 @@
-// Any custom js can go here
+// set up an ad closer click handler
+// that slides up the add on click
+$(".ad-wrapper").on("click", ".closer", function(e){
+  e.preventDefault();
+  $(this).parents(".ad-wrapper").slideUp();
+});
